de7ed0c6be6fa0381ae3471e8f40f1b3a05e88d6
Vitaliy Kuznetsov <vitaliy.kuznetsov@yahoo.co.uk>
2017-03-03 18:08:27 +0000

    Remove ports exposure from compose file

--- swarm-do-deploy.sh
+++ swarm-do-deploy.sh
@@ -2,3 +2,12 @@
 
 eval $(docker-machine env master-1)
 docker stack deploy --compose-file docker-compose.yml events
+
+docker service create \
+    --name events-proxy \
+    --restart-condition on-failure \
+    --publish mode=host,target=80,published=80 \
+    --publish mode=host,target=443,published=443 \
+    --network events_elasticsearch-net \
+    --constraint node.role==manager \
+    vitalcode/events-proxy
