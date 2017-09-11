c83105606340fa15d0d4764d51e71bc2dd92bf7b
Vitaliy Kuznetsov <vitaliy.kuznetsov@yahoo.co.uk>
2017-09-11 19:18:46 +0100

    Deploy books project to digitalocean by travis CI

---
+++ deploy-books.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#!/bin/bash
+
+./swarm-do-start
+./swarm-do-books-deploy.sh
+echo "======>>>>>> Books was deployed"
---
+++ swarm-do-books-deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+eval $(docker-machine env master-1)
+docker stack deploy --compose-file docker-compose-books.yml books
+
+#docker service create \
+#    --name events-proxy \
+#    --restart-condition on-failure \
+#    --publish mode=host,target=80,published=80 \
+#    --publish mode=host,target=443,published=443 \
+#    --network events_events-network \
+#    --constraint node.role==manager \
+#    vitalcode/events-proxy
