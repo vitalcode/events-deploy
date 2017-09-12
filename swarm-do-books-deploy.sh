8b3c20fdd002d68f1c617572e9de23a4d7ed2973
Vitaliy Kuznetsov <vitaliy.kuznetsov@yahoo.co.uk>
2017-09-12 19:50:26 +0100

    Deploy books application to newly build unique swarm cluster

--- swarm-do-books-deploy.sh
+++ swarm-do-books-deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-eval $(docker-machine env master-1)
+PREFIX=$1
+MASTER_NODE=${PREFIX}-master-1
+
+eval $(docker-machine env $MASTER_NODE)
 docker stack deploy --compose-file docker-compose-books.yml books
 
 #docker service create \
--- swarm-do-deploy.sh
+++ swarm-do-deploy.sh
@@ -3,7 +3,7 @@
 PREFIX=$1
 MASTER_NODE=${PREFIX}-master-1
 
-eval $(docker-machine env MASTER_NODE)
+eval $(docker-machine env $MASTER_NODE)
 docker stack deploy --compose-file docker-compose.yml events
 
 docker service create \
